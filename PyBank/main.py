326b8a7ee4093e18563e0770774ff426f652d426
jc411911 <joealmendarez02@gmail.com>
2024-07-11 23:16:55 -0500

    Main Updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,14 @@
-ECHO is on.
+import os
+import csv
+
+budget = os.path.join("Resources", "budget_data.csv")
+
+with open(budget, 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    
+    total_months = 0
+    for row in csv_reader:
+        total_months += 1
+
+print("The total number of months = " + str(total_months))
